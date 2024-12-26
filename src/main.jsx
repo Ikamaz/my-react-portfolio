b5d2c1332db05867463c826125fc678b82760163
Ikamaz <mazmishviliika@gmail.com>
2024-12-26 18:44:50 +0400

    tailwind added and links

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// CSS STYLING
 import './index.css'
+
+// COMPONENTS
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
